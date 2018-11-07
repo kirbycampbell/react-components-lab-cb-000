9e207ebee17b228a080dbb07590d968d696bfab3
Kirby Campbell <jkirbycampbell@gmail.com>
2018-11-07 04:26:58 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -12,12 +12,18 @@ const BUTCHER_PRODUCTS = [
 export class OlderCoaster extends Component {
 
   // this function should return the JSX we want the DOM to render
-  // render() {
-  //   return (
-  //     <div>Hello!</div>
-  //   )
-  // }
-  //
+   render() {
+     return (
+       <div>
+       <p>Two grannies having the time of their life!</p>
+       <p>Passengers:</p>
+      <ul>
+        <li>Agnes</li>
+        <li>Muriel</li>
+      </ul></div>
+     )
+   }
+
 }
 
 export class InFrontOfYou extends Component {
