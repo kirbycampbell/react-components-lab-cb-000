8511f394c2083273248267db736b4ee3138f7449
Kirby Campbell <jkirbycampbell@gmail.com>
2018-11-07 04:32:59 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const BUTCHER_PRODUCTS = [
 ]
 
 // Define these exported classes
-export class OlderCoaster extends Component {
+class OlderCoaster extends Component {
 
   // this function should return the JSX we want the DOM to render
    render() {
